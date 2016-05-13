6a0f7f0eee1760c5d91c6d784b046508c1de2ef3
James Elliott <james@deepsymmetry.org>
2016-05-13 00:45:23 -0500

    Set up for initial release.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,18 @@
 # Change Log
-All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
-## Unreleased
+All notable changes to this project will be documented in this file.
+This change log follows the conventions of
+[keepachangelog.com](http://keepachangelog.com/).
+
+## [Unreleased][unreleased]
+
+Nothing so far.
+
+## 0.1.0 - 2016-05-13
 
 ### Added
 
 - Set up initial project structure.
 - Selector to choose MIDI output as trigger destination.
 
+[unreleased]: https://github.com/brunchboy/beat-link/compare/v0.1.0...HEAD
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject beat-link-trigger "0.1.0-SNAPSHOT"
+(defproject beat-link-trigger "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -17,6 +17,14 @@
 
   :main beat-link-trigger.core
   :uberjar-name "beat-link-trigger.jar"
+
+  ;; Add project name and version information to jar file manifest
+  :manifest {"Name" ~#(str (clojure.string/replace (:group %) "." "/")
+                            "/" (:name %) "/")
+             "Package" ~#(str (:group %) "." (:name %))
+             "Specification-Title" ~#(:name %)
+             "Specification-Version" ~#(:version %)}
+
   :min-lein-version "2.0.0"
 )
 
