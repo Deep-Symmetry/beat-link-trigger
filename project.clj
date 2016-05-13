c21e97f378d5604bd77e024c1cb763d5f7876c24
James Elliott <james@deepsymmetry.org>
2016-05-13 00:53:15 -0500

    Prepare for further development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject beat-link-trigger "0.1.0"
+(defproject beat-link-trigger "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
