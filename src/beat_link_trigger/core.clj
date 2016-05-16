64f5aa73bdc4958bfc1e772bce5d372dbed49993
James Elliott <james@deepsymmetry.org>
2016-05-15 22:21:40 -0500

    Support multiple trigger windows, closes #2

--- src/beat_link_trigger/core.clj
+++ src/beat_link_trigger/core.clj
@@ -48,33 +48,68 @@
   opened, keyed by their names, so we can reuse them."}
   opened-outputs (atom {}))
 
-(def root
-  "The main frame of the user interface"
-  (seesaw/frame :title "Beat Link Trigger" :on-close :dispose))
+(declare create-trigger-window)
+
+(def new-trigger-action
+  "The menu action which opens a new Beat Link Trigger window."
+  (seesaw/action :handler (fn [e] (create-trigger-window))
+                 :name "New Trigger"
+                 :key "menu T"))
+
+(defonce ^{:private true
+           :doc "Holds a map whose keys are the currently-open trigger
+  windows, and whose values are true if the player they are monitoring
+  has been reported as active."}
+  open-triggers (atom {}))
+
+(defn- create-trigger-window
+  "Create a new Beat Link Trigger window."
+  []
+  (let [root (seesaw/frame :title "Beat Link Trigger" :on-close :dispose
+                           :menubar (seesaw/menubar :items [(seesaw/menu :text "Window" :items [new-trigger-action])]))
+        panel (mig/mig-panel
+               :id :panel
+               :items [["Watch:" "alignx trailing"]
+                       [(seesaw/combobox :model (get-player-numbers) :id :players) "wrap"]
+
+                       ["MIDI Output:" "alignx trailing"]
+                       [(seesaw/combobox :model (map :name (get-midi-outputs)) :id :outputs)]
+
+                       ["Message:" "gap unrelated, alignx trailing"]
+                       [(seesaw/combobox :model ["Note" "CC"] :id :message)]
+
+                       [(seesaw/spinner :model (seesaw/spinner-model 127 :from 1 :to 127) :id :note) "wrap"]])]
+    (seesaw/config! root :content panel)
+    (seesaw/pack! root)
+    (seesaw/listen root :window-closed (fn [e]  ; Clean up when we are closed
+                                         (swap! open-triggers dissoc root)))
+    (swap! open-triggers assoc root false)
+    (seesaw/show! root)))
 
 (defn- midi-environment-changed
   "Called when CoreMidi4J reports a change to the MIDI environment, so we can update the menu of
   available MIDI outputs."
   []
   (seesaw/invoke-later  ; Need to move to the AWT event thread, since we interact with GUI objects
-   (let [output-menu (seesaw/select root [:#outputs])
-        old-selection (seesaw/selection output-menu)
-        new-outputs (map :name (get-midi-outputs))]
-     (seesaw/config! output-menu :model new-outputs)  ; Update the content of the output menu
+   (let [new-outputs (map :name (get-midi-outputs))]
+     (doseq [trigger (keys @open-triggers)]  ; Update the output menus in all trigger windows
+       (let [output-menu (seesaw/select trigger [:#outputs])
+             old-selection (seesaw/selection output-menu)]
+         (seesaw/config! output-menu :model new-outputs)  ; Update the content of the output menu
+
+         ;; If our old output selection is still available, restore it
+         (when ((set new-outputs) old-selection)
+           (seesaw/selection! output-menu old-selection))))
 
      ;; Remove any opened outputs that are no longer available in the MIDI environment
-     (swap! opened-outputs #(apply dissoc % (clojure.set/difference (set (keys %)) (set new-outputs))))
-
-     ;; If our old output selection is still available, restore it
-     (when ((set new-outputs) old-selection)
-       (seesaw/selection! output-menu old-selection)))))
+     (swap! opened-outputs #(apply dissoc % (clojure.set/difference (set (keys %)) (set new-outputs)))))))
 
 (defn- get-chosen-output
-  "Return the MIDI output to which messages should be sent, opening it
-  if this is the first time we are using it, or reusing it if we
-  already opened it."
-  []
-  (let [output-menu (seesaw/select root [:#outputs])
+  "Return the MIDI output to which messages should be sent for a given
+  trigger, opening it if this is the first time we are using it, or
+  reusing it if we already opened it."
+  [trigger]
+  (let [output-menu (seesaw/select trigger [:#outputs])
         selection (seesaw/selection output-menu)]
     (or (get @opened-outputs selection)
         (let [new-output (midi/midi-out selection)]
@@ -82,52 +117,51 @@
           new-output))))
 
 (defn- report-activation
-  "Send a message indicating the player we are watching has started playing."
-  []
-  (let [note (seesaw/value (seesaw/select root [:#note]))]
-    (if (= "Note" (seesaw/value (seesaw/select root [:#message])))
-      (midi/midi-note-on (get-chosen-output) note 127)
-      (midi/midi-control (get-chosen-output) note 127))))
+  "Send a message indicating the player a trigger is watching has
+  started playing."
+  [trigger]
+  (let [note (seesaw/value (seesaw/select trigger [:#note]))]
+    (if (= "Note" (seesaw/value (seesaw/select trigger [:#message])))
+      (midi/midi-note-on (get-chosen-output trigger) note 127)
+      (midi/midi-control (get-chosen-output trigger) note 127))))
 
 (defn- report-deactivation
-  "Send a message indicating the player we are watching has started playing."
-  []
-  (let [note (seesaw/value (seesaw/select root [:#note]))]
-    (if (= "Note" (seesaw/value (seesaw/select root [:#message])))
-      (midi/midi-note-off (get-chosen-output) note)
-      (midi/midi-control (get-chosen-output) note 0))))
-
-(defonce ^{:private true
-           :doc "Will hold a true value when we have sent a trigger indicating our
-  tracked device is active."}
-  reported-active (atom false))
+  "Send a message indicating the player a trigger is watching has
+  started playing."
+  [trigger]
+  (let [note (seesaw/value (seesaw/select trigger [:#note]))]
+    (if (= "Note" (seesaw/value (seesaw/select trigger [:#message])))
+      (midi/midi-note-off (get-chosen-output trigger) note)
+      (midi/midi-control (get-chosen-output trigger) note 0))))
 
 (defn- update-device-state
-  "If the device state has changed, send an appropriate message and record it."
-  [new-state]
-  (swap! reported-active
-         (fn [was-active]
-           (when (not= new-state was-active)
-             (if new-state (report-activation) (report-deactivation)))
-           new-state)))
+  "If the device state being watched by a trigger has changed, send an
+  appropriate message and record the new state."
+  [trigger new-state]
+  (swap! open-triggers
+         (fn [triggers]
+           (when (not= new-state (get triggers trigger))
+             (if new-state (report-activation trigger) (report-deactivation trigger)))
+           (assoc triggers trigger new-state))))
 
 (defn- rebuild-player-menu
   "Updates the combo box of available players to reflect the ones
   currently found on the network. If the previously selected player
   has disappeared, so we should deactivate it if it had been active."
   []
-  (let [player-menu (seesaw/select root [:#players])
-        old-selection (seesaw/selection player-menu)
-        new-options (get-player-numbers)]
-    (seesaw/config! player-menu :model new-options)
-    (if ((set new-options) old-selection)
-      (seesaw/selection! player-menu old-selection)  ; Old selection still available, restore it
-      (update-device-state false)))) ; Selected player disappeared, need to deactivate it if it was active
+  (let [new-options (get-player-numbers)]
+    (doseq [trigger (keys @open-triggers)]
+      (let [player-menu (seesaw/select trigger [:#players])
+            old-selection (seesaw/selection player-menu)]
+        (seesaw/config! player-menu :model new-options)
+        (if ((set new-options) old-selection)
+          (seesaw/selection! player-menu old-selection)  ; Old selection still available, restore it
+          (update-device-state trigger false)))))) ; Selected player disappeared, need to deactivate if it was active
 
 
 (defonce ^{:private true
-           :doc "Responds to the arrival or departure of DJ Link devices by updating
-  our map and user interface appropriately."}
+           :doc "Responds to the arrival or departure of DJ Link
+  devices by updating our user interface appropriately."}
   device-listener
   (reify org.deepsymmetry.beatlink.DeviceAnnouncementListener
     (deviceFound [this announcement]
@@ -136,15 +170,16 @@
       (rebuild-player-menu))))
 
 (defonce ^{:private true
-           :doc "Responds to player status updates and tracks the
-  state of the one we are watching."}
+           :doc "Responds to player status updates and updates the
+  state of any triggers watching them."}
   status-listener
   (reify org.deepsymmetry.beatlink.DeviceUpdateListener
     (received [this status]
-(let [player-menu (seesaw/select root [:#players])
-      selection (seesaw/selection player-menu)]
-  (when (and (instance? CdjStatus status) (= selection (str "Player " (.getDeviceNumber status))))
-    (update-device-state (.isPlaying status)))))))
+      (doseq [trigger (keys @open-triggers)]
+        (let [player-menu (seesaw/select trigger [:#players])
+              selection (seesaw/selection player-menu)]
+          (when (and (instance? CdjStatus status) (= selection (str "Player " (.getDeviceNumber status))))
+            (update-device-state trigger (.isPlaying status))))))))
 
 (defn- searching-frame
   "Create and show a frame that explains we are looking for devices."
@@ -183,22 +218,8 @@
        (midiSystemUpdated [this]
          (midi-environment-changed)))))
 
-  ;; Build the UI
-  (let [panel (mig/mig-panel
-               :id :panel
-               :items [["Watch:" "alignx trailing"]
-                       [(seesaw/combobox :model (get-player-numbers) :id :players) "wrap"]
-                       
-                       ["MIDI Output:" "alignx trailing"]
-                       [(seesaw/combobox :model (map :name (get-midi-outputs)) :id :outputs)]
-                       
-                       ["Message:" "gap unrelated, alignx trailing"]
-                       [(seesaw/combobox :model ["Note" "CC"] :id :message)]
-                       
-                       [(seesaw/spinner :model (seesaw/spinner-model 127 :from 1 :to 127) :id :note) "wrap"]])]
-    (seesaw/config! root :content panel)
-    (seesaw/pack! root)
-    (seesaw/show! root))
+  ;; Open the first trigger window
+  (create-trigger-window)
 
   (DeviceFinder/addDeviceAnnouncementListener device-listener)  ; Be able to react to players coming and going
   (rebuild-player-menu)  ; In case we missed an update between creating the UI and registering the listener
