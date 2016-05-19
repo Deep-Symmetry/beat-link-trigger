e44cd9e9b2b97c250cd71053b30526b214ba899d
James Elliott <james@deepsymmetry.org>
2016-05-19 16:15:51 -0500

    Update dependency.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [environ "1.0.2"]
+                 [environ "1.0.3"]
                  [org.deepsymmetry/beat-link "0.1.4"]
                  [overtone/midi-clj "0.5.0"]
                  [overtone/osc-clj "0.9.0"]
