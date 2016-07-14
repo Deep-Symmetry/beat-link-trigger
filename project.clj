b9924ebf920935547f40840fd761b4a860220a92
James Elliott <james@deepsymmetry.org>
2016-07-13 21:42:54 -0500

    Update dependency.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.fifesoft/rsyntaxtextarea "2.5.8"]
                  [com.github.insubstantial/substance "7.3"]
-                 [com.taoensso/timbre "4.5.1"]
+                 [com.taoensso/timbre "4.6.0"]
                  [com.fzakaria/slf4j-timbre "0.3.2"]
                  [environ "1.0.3"]
                  [fipp "0.6.5"]
