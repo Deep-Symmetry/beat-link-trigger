48a9512e4d83c2ed4d28aebb0ebbc9f2669b0bf1
James Elliott <james@deepsymmetry.org>
2019-11-20 18:14:02 -0600

    Initial attempt to support XDJ-XZ

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                  [fipp "0.6.21"]
                  [inspector-jay "0.3" :exclusions [org.clojure/core.memoize]]
                  [me.raynes/fs "1.4.6"]
-                 [org.deepsymmetry/beat-link "0.5.5-SNAPSHOT"]
+                 [org.deepsymmetry/beat-link "0.6.0-SNAPSHOT"]
                  [org.deepsymmetry/electro "0.1.3"]
                  [overtone/midi-clj "0.5.0" :exclusions [overtone/at-at]]
                  [overtone/osc-clj "0.9.0"]
@@ -32,7 +32,7 @@
                  [nrepl "0.6.0"]
                  [cider/cider-nrepl "0.22.4"]
                  [com.cemerick/pomegranate "1.1.0"]
-                 [org.apache.maven/maven-artifact "3.5.3"]]
+                 [org.apache.maven/maven-artifact "3.6.2"]]
   :repositories {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
 
   :profiles {:dev     {:repl-options {:init-ns beat-link-trigger.core
