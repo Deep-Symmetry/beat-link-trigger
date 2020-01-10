61f496e67228b948b6af4d795e75af3343cea3f1
James Elliott <james@deepsymmetry.org>
2020-01-10 00:31:39 -0600

    Refactor, improve, and document the build workflow.
    * Move values shared between jobs and steps to top-level environment
      variables, especially things like the copyright date which will
      change.
    * Stop checking for or deleting files we know won't exist.
    * Only attempt to code-sign the Mac installer if the necessary secrets
      are present in the repository. This will allow forks to at least
      build without error, albeit unsigned.
    * Add a lot of comments explaining why things are done.

--- .github/scripts/build_msi.ps1
+++ .github/scripts/build_msi.ps1
@@ -1,3 +1,6 @@
+# Download and expand the Amazon Corretto 11 JDK, then use it to build the embedded JRE for inside
+# the Mac application. But if it already exists (because we use a cache action to speed things up),
+# we can skip this section.
 If (! (Test-Path "Runtime")) {
     Invoke-WebRequest https://corretto.aws/downloads/latest/amazon-corretto-11-x64-windows-jdk.zip `
       -OutFile jdk11.zip
@@ -5,25 +8,22 @@ If (! (Test-Path "Runtime")) {
     $jdk11Subdir = (Get-ChildItem -Path jdk11 -name)
     $jlink = ".\jdk11\$jdk11Subdir\bin\jlink"
       & $jlink --no-header-files --no-man-pages --compress=2 --strip-debug `
-        --add-modules=java.base,java.desktop,java.management,java.naming,java.prefs,java.sql,jdk.zipfs,jdk.unsupported `
-        --output .\Runtime
+        --add-modules="$env:blt_java_modules" --output .\Runtime
 }
 
-If (! (Test-path "Input")) {
-  mkdir Input
-}
-
+# Move the downloaded cross-platform executable Jar into an Input folder to be used in building the
+# native app bundle.
+mkdir Input
 mv beat-link-trigger.jar Input
-del *.msi
 
-jpackage --name "Beat Link Trigger" --input .\Input `
- --runtime-image .\Runtime `
+# Build the native application bundle and installer.
+jpackage --name "$env:blt_name" --input .\Input --runtime-image .\Runtime `
  --icon ".\.github\resources\BeatLink.ico" `
  --main-jar beat-link-trigger.jar `
  --win-menu --win-menu-group "Deep Symmetry" --type msi `
  --win-upgrade-uuid 6D58C8D7-6163-43C6-93DC-A4C8CC1F81B6 `
- --description "Trigger events and automate shows in response to events on Pioneer CDJs" `
- --copyright "© 2016-2020 Deep Symmetry, LLC" --vendor "Deep Symmetry, LLC" `
+ --description "$env:blt_description" --copyright "$env:blt_copyright" --vendor "$env:blt_vendor" `
  --app-version "$env:version_tag"
 
+# Rename the installer file to the name we like to use for the release artifact.
 mv "$env:msi_name" "$env:artifact_name"
--- .github/scripts/build_pkg.zsh
+++ .github/scripts/build_pkg.zsh
@@ -1,3 +1,6 @@
+# Download and expand the Amazon Corretto 11 JDK, then use it to build the embedded JRE for inside
+# the Mac application. But if it already exists (because we use a cache action to speed things up),
+# we can skip this section.
 if [ ! -d Runtime ]; then
     curl --location https://corretto.aws/downloads/latest/amazon-corretto-11-x64-macos-jdk.tar.gz \
          --output runtime.tar.gz
@@ -7,25 +10,45 @@ if [ ! -d Runtime ]; then
         --output Runtime
 fi
 
+# Move the downloaded cross-platform executable Jar into an Input folder to be used in building the
+# native app bundle.
 mkdir Input
 mv beat-link-trigger.jar Input
 
-security create-keychain -p "$IDENTITY_PASSPHRASE" build.keychain
-security default-keychain -s build.keychain
-security unlock-keychain -p "$IDENTITY_PASSPHRASE" build.keychain
+# See if the secrets needed to code-sign the native application are present.
+if  [ "$IDENTITY_PASSPHRASE" != "" ]; then
 
-echo "$IDENTITY_P12_B64" > DS_ID_App.p12.txt
-openssl base64 -d -in DS_ID_App.p12.txt -out DS_ID_App.p12
-security import DS_ID_App.p12 -A -P "$IDENTITY_PASSPHRASE"
+    # We have secrets! Set up a keychain to hold the signing certificate. We can use the same
+    # secret passphrase that we will use to import it as the keychain password, for simplicity.
+    security create-keychain -p "$IDENTITY_PASSPHRASE" build.keychain
+    security default-keychain -s build.keychain
+    security unlock-keychain -p "$IDENTITY_PASSPHRASE" build.keychain
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "$IDENTITY_PASSPHRASE" build.keychain
+    # Put the base-64 encoded signing certificicate into a text file, decode it to binary form.
+    echo "$IDENTITY_P12_B64" > DS_ID_App.p12.txt
+    openssl base64 -d -in DS_ID_App.p12.txt -out DS_ID_App.p12
 
-jpackage --name "Beat Link Trigger" --input Input --runtime-image Runtime \
-         --icon .github/resources/BeatLink.icns --main-jar beat-link-trigger.jar \
-         --description "Trigger events and automate shows in response to events on Pioneer CDJs" \
-         --copyright "© 2016-2020 Deep Symmetry, LLC" --vendor "Deep Symmetry, LLC" \
-         --type dmg --mac-package-identifier "org.deepsymmetry.beat-link-trigger" \
-         --mac-sign --mac-signing-key-user-name "Deep Symmetry, LLC (9M6LKU948Y)" \
-         --app-version $version_tag
+    # Install the decoded signing certificate into our unlocked build keychain.
+    security import DS_ID_App.p12 -A -P "$IDENTITY_PASSPHRASE"
 
+    # Set the keychain to allow use of the certificate without user interaction (we are headless!)
+    security set-key-partition-list -S apple-tool:,apple: -s -k "$IDENTITY_PASSPHRASE" build.keychain
+
+    # Finally, run jpackage to build the native application as a code-signed disk image.
+    jpackage --name $blt_name --input Input --runtime-image Runtime \
+             --icon .github/resources/BeatLink.icns --main-jar beat-link-trigger.jar \
+             --description $blt_description --copyright $blt_copyright --vendor $blt_vendor \
+             --type dmg --mac-package-identifier "org.deepsymmetry.beat-link-trigger" \
+             --mac-sign --mac-signing-key-user-name $blt_mac_signing_name \
+             --app-version $version_tag
+else
+    # We have no secrets, so build the native application disk image without code signing.
+    jpackage --name $blt_name --input Input --runtime-image Runtime \
+             --icon .github/resources/BeatLink.icns --main-jar beat-link-trigger.jar \
+             --description $blt_description --copyright $blt_copyright --vendor $blt_vendor \
+             --type dmg --mac-package-identifier "org.deepsymmetry.beat-link-trigger" \
+             --app-version $version_tag
+fi
+
+# Rename the disk image to the name we like to use for the release artifact.
 mv "$dmg_name" "$artifact_name"
