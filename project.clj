c373af04a5c5936e68e05b28b7faa8fdc5e37969
James Elliott <james@deepsymmetry.org>
2020-01-12 01:15:47 -0600

    Update dependencies.

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :aot [beat-link-trigger.BeatLinkTrigger beat-link-trigger.TexturedRaven]
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "0.6.532"]
+                 [org.clojure/core.async "0.7.559"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/tools.cli "0.4.2"]
                  [clojure-humanize "0.2.2"]
-                 [com.fifesoft/rsyntaxtextarea "3.0.4"]
+                 [com.fifesoft/rsyntaxtextarea "3.0.8"]
                  [com.fifesoft/rstaui "3.0.2"]
                  [org.pushing-pixels/radiance-substance "1.0.2"]
                  [org.pushing-pixels/radiance-substance-extras "1.0.2"]
