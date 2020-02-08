b1c0edee320d3a411c90eddcdb6d45027c93a013
James Elliott <james@deepsymmetry.org>
2020-02-08 17:54:43 -0600

    Try new notarization flow with pre-notarized OpenJDK 14 runtime.

--- .github/scripts/build_dmg.zsh
+++ .github/scripts/build_dmg.zsh
@@ -1,13 +1,7 @@
-# Download and expand the Amazon Corretto 11 JDK, then use it to build the embedded JRE for inside
-# the Mac application. But if it already exists (because we use a cache action to speed things up),
-# we can skip this section.
-if [ ! -d Runtime ]; then
-    curl --location https://corretto.aws/downloads/latest/amazon-corretto-11-x64-macos-jdk.tar.gz \
-         --output runtime.tar.gz
-    tar xvf runtime.tar.gz
-    amazon-corretto-11.jdk/Contents/Home/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug \
-        --add-modules="$blt_java_modules" --output Runtime
-fi
+# Build the embedded JRE for inside the Mac application, using the OpenJDK 14 the workflow
+# set up, because that is already properly notarized, unlike Amazon Corretto 11.
+jlink --no-header-files --no-man-pages --compress=2 --strip-debug \
+      --add-modules="$blt_java_modules" --output Runtime
 
 # Move the downloaded cross-platform executable Jar into an Input folder to be used in building the
 # native app bundle.
@@ -33,31 +27,35 @@ if  [ "$IDENTITY_PASSPHRASE" != "" ]; then
     # Set the keychain to allow use of the certificate without user interaction (we are headless!)
     security set-key-partition-list -S apple-tool:,apple: -s -k "$IDENTITY_PASSPHRASE" build.keychain
 
-    # Run jpackage to build the native application as a disk image.
+    # Run jpackage to build the native application as an application image so we can fix code signing issues.
     jpackage --name $blt_name --input Input --runtime-image Runtime \
              --icon .github/resources/BeatLink.icns --main-jar beat-link-trigger.jar \
              --description $blt_description --copyright $blt_copyright --vendor $blt_vendor \
-             --type dmg --mac-package-identifier "org.deepsymmetry.beat-link-trigger" \
+             --type app-image --mac-package-identifier "org.deepsymmetry.beat-link-trigger" \
              --app-version $build_version
 
-    # Code sign the disk image more robustly than jpackage is currently able to, for Catalina.
-    echo "Prior to code signing:"
-    ls -l "$dmg_name"
-    codesign -dv --verbose=4 "$dmg_name"
-    echo
-    spctl -a -vv -t install "$dmg_name"
+    # Remove the extra copy of libjli.dylib which causes notarization to fail
+    rm -r "$blt_name.app/Contents/runtime/Contents/MacOS/"
 
+    # Code sign the application image more robustly than jpackage is currently able to, for Catalina.
+    echo "Code signing the application image."
+    codesign --force --timestamp --options runtime \
+             --verbose=4 --entitlements .github/resources/Clojure.entitlements \
+             --prefix "org.deepsymmetry.beat-link-trigger." \
+             --sign "$blt_mac_signing_name" "$blt_name.app"
+
+    # Create a disk image that contains just the application image.
+    echo "Creating the disk image."
+    mkdir Output
+    mv "$blt_name.app" Output
+    hdiutil create -volname "$blt_name" -srcfolder Output/ -ov -format UDZO "$dmg_name"
+
+    # Code sign the disk image as well.
     echo "Code signing the disk image."
-    codesign --force --preserve-metadata=identifier,requirements --deep --timestamp --options runtime \
+    codesign --timestamp --options runtime \
              --verbose=4 --entitlements .github/resources/Clojure.entitlements \
              --prefix "org.deepsymmetry.beat-link-trigger." \
              --sign "$blt_mac_signing_name" "$dmg_name"
-
-    echo "After code signing:"
-    ls -l "$dmg_name"
-    codesign -dv --verbose=4 "$dmg_name"
-    echo
-    spctl -a -vv -t install "$dmg_name"
 
     # Submit the disk image to Apple for notarization.
     echo "Sumbitting the disk image to Apple for notarization..."
