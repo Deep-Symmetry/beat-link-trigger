0a3d41da38dcb4801d6535eefc86a5a7cd69fad8
James Elliott <james@deepsymmetry.org>
2020-02-12 17:46:14 -0600

    Temporarily remove notarization surgery
    This will cause the notarization step to fail, but I am working with
    Apple Developer Support to undrstand why, so I can help the OpenJDK
    team fix jpackage, and stop having to do this hackery myself.

--- .github/scripts/build_dmg.zsh
+++ .github/scripts/build_dmg.zsh
@@ -43,7 +43,8 @@ if  [ "$IDENTITY_PASSPHRASE" != "" ]; then
              --app-version $build_version
 
     # Remove the extra copy of libjli.dylib which causes notarization to fail
-    rm -r "$blt_name.app/Contents/runtime/Contents/MacOS/"
+    # TEMPORARY: Commented out for sharing with Apple Developer Support, this will fail!
+    #rm -r "$blt_name.app/Contents/runtime/Contents/MacOS/"
 
     # Code sign the application image more robustly than jpackage is currently able to, for Catalina.
     echo "Code signing the application image."
