ab5a769096f08fdb29c1bb2e021415cd0106b921
James Elliott <james@deepsymmetry.org>
2020-05-06 18:16:32 -0500

    Switch to netlify.toml build with htmltest link checking.

---
+++ doc/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This script is run by Netlify continuous integration to build the
+# Antora site hosting the DJ Link packet analysis and the devicesql
+# database analysis.
+
+npm i @antora/cli antora-site-generator-lunr
+DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr $(npm bin)/antora --fetch --generator antora-site-generator-lunr \
+  doc/netlify.yml
+
+curl https://htmltest.wjdp.uk | bash
+bin/htmltest
