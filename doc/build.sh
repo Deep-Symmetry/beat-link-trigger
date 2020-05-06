7e4f31406b2bc2cd57632cb06c6014751c7d866b
James Elliott <james@deepsymmetry.org>
2020-05-06 18:35:03 -0500

    Fix comment. [skip ci]

--- doc/build.sh
+++ doc/build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # This script is run by Netlify continuous integration to build the
-# Antora site hosting the DJ Link packet analysis and the devicesql
-# database analysis.
+# Antora site hosting the user guide.
 
 npm i @antora/cli antora-site-generator-lunr
 DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr $(npm bin)/antora --fetch --generator antora-site-generator-lunr \
