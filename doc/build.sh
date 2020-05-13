e8eb1bd8aa9e6e3ee2e8219825163e0ae345f2e8
James Elliott <james@deepsymmetry.org>
2020-05-12 20:24:46 -0500

    Index only the latest version of the user guide.
    Thanks to a new antora-site-generator-lunr feature, w00t!

--- doc/build.sh
+++ doc/build.sh
@@ -4,8 +4,8 @@
 # Antora site hosting the user guide.
 
 npm i @antora/cli antora-site-generator-lunr
-DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr $(npm bin)/antora --fetch --generator antora-site-generator-lunr \
-  doc/netlify.yml
+DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr DOCSEARCH_INDEX_VERSION=latest \
+  $(npm bin)/antora --fetch --generator antora-site-generator-lunr doc/netlify.yml
 
 curl https://htmltest.wjdp.uk | bash
 bin/htmltest
