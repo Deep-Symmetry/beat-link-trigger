5bd978ff2e65adc8efdb6c8c90e83e60923f8f95
James Elliott <james@deepsymmetry.org>
2020-08-17 23:23:50 -0500

    Got settings driver working for first two settings [skip ci]

--- src/beat_link_trigger/settings_loader.clj
+++ src/beat_link_trigger/settings_loader.clj
@@ -1,14 +1,16 @@
 (ns beat-link-trigger.settings-loader
   "Provides the user inerface for configuring a My Settings profile and
   sending it to players found on the network."
-  (:require [beat-link-trigger.track-loader :as track-loader]
+  (:require [beat-link-trigger.prefs :as prefs]
+            [beat-link-trigger.track-loader :as track-loader]
             [seesaw.core :as seesaw]
             [seesaw.mig :as mig]
             [taoensso.timbre :as timbre])
   (:import [beat_link_trigger.util PlayerChoice]
            [java.awt.event WindowEvent]
            [org.deepsymmetry.beatlink CdjStatus DeviceAnnouncement DeviceAnnouncementListener DeviceFinder
-            DeviceUpdate DeviceUpdateListener LifecycleListener PlayerSettings VirtualCdj]))
+            DeviceUpdate DeviceUpdateListener LifecycleListener PlayerSettings PlayerSettings$PlayMode
+            PlayerSettings$Toggle VirtualCdj]))
 
 (defonce ^{:private true
            :doc "Holds the frame allowing the user to adjust settings
@@ -26,6 +28,40 @@
   `VirtualCdj`](https://deepsymmetry.org/beatlink/apidocs/org/deepsymmetry/beatlink/VirtualCdj.html)
   singleton."
   (VirtualCdj/getInstance))
+
+(defn- enum-picker
+  "Creates a menu for choosing a setting based on one of the special
+  enums used by `PlayerSettings`, and handles initializing it based on
+  the current preferences, as well as updating the settings object and
+  preferences appropriately when the user chooses a different value."
+  [enum settings defaults field-name]
+  (let [field         (.. settings getClass (getDeclaredField field-name))
+        value-of      (.. enum (getDeclaredMethod "valueOf" (into-array Class [String])))
+        display-value (.. enum (getDeclaredField "displayValue"))]
+
+    ;; If there is a stored preference for this setting, establish it.
+    (when-let [default (get defaults field-name)]
+      (. field (set settings (. value-of (invoke enum (to-array [default]))))))
+
+    ;; Create the combo box with a model that uses the specified enumeration constants,
+    ;; and a custom cell renderer which draws the displayValue, rather than the persistence-oriented
+    ;; toString() value, and install a state change handler which updates both the settings
+    ;; object and the stored preferences values to reflect the user's wish.
+    (let [box  (seesaw/combobox :model (.getEnumConstants enum)
+                                :selected-item (. field (get settings))
+                                :listen [:item-state-changed
+                                         (fn [e]
+                                           (when-let [selection (seesaw/selection e)]
+                                             (. field (set settings selection))
+                                             (prefs/put-preferences (update-in (prefs/get-preferences) [:my-settings]
+                                                                               assoc field-name (str selection)))))])
+          orig (.getRenderer box)
+          draw (proxy [javax.swing.DefaultListCellRenderer] []
+                 (getListCellRendererComponent [the-list value index is-selected has-focus]
+                   (let [display (. display-value (get value))]
+                     (.getListCellRendererComponent orig the-list display index is-selected has-focus))))]
+      (.setRenderer box draw)
+      box)))
 
 (defn- create-window
   "Builds an interface in which the user can adjust their My Settings
@@ -53,7 +89,14 @@
            player-panel     (mig/mig-panel :background "#ddd"
                                            :items [[(seesaw/label :text "Load on:")]
                                                    [players] [load-button] [problem-label "push"]])
-           layout           (seesaw/border-panel :south player-panel)
+           settings         (PlayerSettings.)
+           defaults         (:my-settings (prefs/get-preferences))
+           settings-panel   (mig/mig-panel
+                             :items [[(seesaw/label :text "Play Mode:")]
+                                     [(enum-picker PlayerSettings$PlayMode settings defaults "autoPlayMode") "wrap"]
+                                     [(seesaw/label :text "Eject/Load Lock:")]
+                                     [(enum-picker PlayerSettings$Toggle settings defaults "ejectLoadLock") "wrap"]])
+           layout           (seesaw/border-panel :center settings-panel :south player-panel)
            stop-listener    (reify LifecycleListener
                               (started [this _]) ; Nothing to do, we exited as soon as a stop happened anyway.
                               (stopped [this _]  ; Close our window if VirtualCdj stops (we need it).
@@ -87,9 +130,7 @@
        (seesaw/listen load-button
                       :action-performed
                       (fn [_]
-                        (let [^PlayerSettings settings (PlayerSettings.)
-                              ^Long selected-player    (.number ^PlayerChoice (.getSelectedItem players))]
-                          ;; TODO: Adjust settings based on UI.
+                        (let [^Long selected-player (.number ^PlayerChoice (.getSelectedItem players))]
                           (.sendLoadSettingsCommand virtual-cdj selected-player settings))))
 
        (when-not (.isRunning virtual-cdj)  ; In case it shut down during our setup.
