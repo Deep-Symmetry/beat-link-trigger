2f6804c60ef18513c16b4611cc09d69a7d9f6eed
James Elliott <james@deepsymmetry.org>
2021-01-31 19:14:03 -0600

    Switch to re-versioned Beat Link.

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
                  [fipp "0.6.23"]
                  [inspector-jay "0.3" :exclusions [org.clojure/core.memoize]]
                  [me.raynes/fs "1.4.6"]
-                 [org.deepsymmetry/beat-link "0.7.0-SNAPSHOT"]
+                 [org.deepsymmetry/beat-link "7.0.0-SNAPSHOT"]
                  [org.deepsymmetry/electro "0.1.4"]
                  [beat-carabiner "0.2.3"]
                  [overtone/midi-clj "0.5.0" :exclusions [overtone/at-at]]
