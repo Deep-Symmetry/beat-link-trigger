e4fcf7dc70b303339b63486dc039fea29714b53c
James Elliott <james@deepsymmetry.org>
2023-02-21 00:59:55 -0600

    Try streamlined Mac code signing process with newer JDK

--- .github/scripts/build_dmg.zsh
+++ .github/scripts/build_dmg.zsh
@@ -1,21 +1,6 @@
 # Fail if any step fails
 set -e
 
-# Download and expand the Oracle OpenJDK 14, which is properly notarized.
-# But if it already exists (because we use a cache action to speed things up), we can skip this section.
-if [ ! -d jdk-14.0.1.jdk ]; then
-    echo "Downloding and extracting OpenJDK 14.0.1."
-    curl --location \
-         https://download.java.net/java/GA/jdk14.0.1/664493ef4a6946b186ff29eb326336a2/7/GPL/openjdk-14.0.1_osx-x64_bin.tar.gz \
-         --output runtime.tar.gz
-    tar xvf runtime.tar.gz
-fi
-
-# Use OpenJDK 14 to build the embedded JRE for inside the Mac application.
-echo "Creating optimized OpenJDK runtime for embedding into Beat Link Trigger."
-jdk-14.0.1.jdk/Contents/Home/bin/jlink --no-header-files --no-man-pages --compress=2 --strip-debug \
-  --add-modules="$blt_java_modules" --output Runtime
-
 # Move the downloaded cross-platform executable Jar into an Input folder to be used in building the
 # native app bundle.
 mkdir Input
@@ -40,35 +25,15 @@ if  [ "$IDENTITY_PASSPHRASE" != "" ]; then
     # Set the keychain to allow use of the certificate without user interaction (we are headless!)
     security set-key-partition-list -S apple-tool:,apple: -s -k "$IDENTITY_PASSPHRASE" build.keychain
 
-    # Run jpackage to build the native application as an application image so we can fix code signing issues.
-    jdk-14.0.1.jdk/Contents/Home/bin/jpackage --name $blt_name --input Input --runtime-image Runtime \
-             --icon .github/resources/BeatLink.icns --main-jar beat-link-trigger.jar \
-             --description $blt_description --copyright $blt_copyright --vendor $blt_vendor \
-             --type app-image --mac-package-identifier "org.deepsymmetry.beat-link-trigger" \
-             --app-version $build_version
-
-    # Remove the extra copy of libjli.dylib which causes notarization to fail
-    rm -r "$blt_name.app/Contents/runtime/Contents/MacOS/"
-
-    # Code sign the application image more robustly than jpackage is currently able to, for Catalina.
-    echo "Code signing the application image."
-    codesign --force --timestamp --options runtime \
-             --verbose=4 --entitlements .github/resources/Clojure.entitlements \
-             --prefix "org.deepsymmetry.beat-link-trigger." \
-             --sign "$blt_mac_signing_name" "$blt_name.app"
-
-    # Create a disk image that contains just the application image.
-    echo "Creating the disk image."
-    mkdir Output
-    mv "$blt_name.app" Output
-    hdiutil create -volname "$blt_name" -fs HFS+ -srcfolder Output/ -ov -format UDZO "$dmg_name"
-
-    # Code sign the disk image as well.
-    echo "Code signing the disk image."
-    codesign --timestamp --options runtime \
-             --verbose=4 --entitlements .github/resources/Clojure.entitlements \
-             --prefix "org.deepsymmetry.beat-link-trigger." \
-             --sign "$blt_mac_signing_name" "$dmg_name"
+    # Run jpackage to build the native application as a code signed disk image
+    jpackage --name "$blt_name" --input Input
+      --add-modules java.base,java.desktop,java.management,java.naming,java.prefs,java.sql,jdk.zipfs,jdk.unsupported \
+      --icon .github/resources/BeatLink.icns --main-jar beat-link-trigger.jar \
+      --description "$blt_description" --copyright "$blt_copyright" --vendor "$blt_vendor" \
+      --mac-package-identifier "org.deepsymmetry.beat-link-trigger" --app-version $build_version \
+      --mac-sign --mac-package-signing-prefix "org.deepsymmetry.beat-link-trigger." \
+      --mac-signing-key-user-name  "Deep Symmetry, LLC (9M6LKU948Y)" \
+      --mac-entitlements  .github/resources/Clojure.entitlements
 
     # Submit the disk image to Apple for notarization.
     echo "Sumbitting the disk image to Apple for notarization..."
@@ -99,11 +64,11 @@ if  [ "$IDENTITY_PASSPHRASE" != "" ]; then
 
 else
     # We have no secrets, so build the native application disk image without code signing.
-    jpackage --name $blt_name --input Input --runtime-image Runtime \
-             --icon .github/resources/BeatLink.icns --main-jar beat-link-trigger.jar \
-             --description $blt_description --copyright $blt_copyright --vendor $blt_vendor \
-             --type dmg --mac-package-identifier "org.deepsymmetry.beat-link-trigger" \
-             --app-version $build_version
+    jpackage --name "$blt_name" --input Input
+      --add-modules java.base,java.desktop,java.management,java.naming,java.prefs,java.sql,jdk.zipfs,jdk.unsupported \
+      --icon .github/resources/BeatLink.icns --main-jar beat-link-trigger.jar \
+      --description "$blt_description" --copyright "$blt_copyright" --vendor "$blt_vendor" \
+      --mac-package-identifier "org.deepsymmetry.beat-link-trigger" --app-version $build_version
 fi
 
 # Rename the disk image to the name we like to use for the release artifact.
