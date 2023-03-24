be607ba72a5e86cd0b22004550389133441ba4f8
James Elliott <james@deepsymmetry.org>
2023-03-24 07:50:16 -0500

    Auto-dismiss modals on focus rather than blur, #158

--- src/beat_link_trigger/show_util.clj
+++ src/beat_link_trigger/show_util.clj
@@ -1114,7 +1114,7 @@
          (str "var showFile='" (hiccup.util/url-encode path) "';\n")
          "hljs.highlightAll();"]
         (page/include-js "/resources/expression-report.js")
-        [:body {:onblur "closeAllModals();"}
+        [:body {:onfocus "closeAllModals();"}
          [:section.section
           [:div.container
            [:h1.title "Expressions in Show " [:span.has-text-primary path]]
