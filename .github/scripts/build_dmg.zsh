b759f190a8580945f1bd7a8ed088f51a6f05a9ec
James Elliott <james@deepsymmetry.org>
2025-05-25 17:09:25 -0500

    Working on macOS code signing issues #198

--- .github/scripts/build_dmg.zsh
+++ .github/scripts/build_dmg.zsh
@@ -35,7 +35,6 @@ if  [ "$IDENTITY_PASSPHRASE" != "" ]; then
     codesign --timestamp -s "Deep Symmetry, LLC (9M6LKU948Y)" libnrepl-macos-universal.so
     codesign --timestamp -s "Deep Symmetry, LLC (9M6LKU948Y)" META-INF/native/libnetty_transport_native_kqueue_x86_64.jnilib
     codesign --timestamp -s "Deep Symmetry, LLC (9M6LKU948Y)" --force uk/co/xfactorylibrarians/coremidi4j/libCoreMidi4J.dylib
-    codesign --timestamp -s "Deep Symmetry, LLC (9M6LKU948Y)" com/sun/jna/darwin/libjnidispatch.jnilib
     codesign --timestamp -s "Deep Symmetry, LLC (9M6LKU948Y)" --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
     codesign --timestamp -s "Deep Symmetry, LLC (9M6LKU948Y)" --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
     codesign --timestamp -s "Deep Symmetry, LLC (9M6LKU948Y)" com/formdev/flatlaf/natives/libflatlaf-macos-x86_64.dylib
