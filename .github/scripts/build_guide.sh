fbc3e12444323db6166575075f6fe882e5ccfb7a
James Elliott <james@deepsymmetry.org>
2025-07-14 16:15:08 -0500

    Consolidate online and PDF builds
    Also fix a lot of cross-references for the PDF.

--- .github/scripts/build_guide.sh
+++ .github/scripts/build_guide.sh
@@ -8,9 +8,11 @@ npm install
 # There is no point in doing this if we lack the SSH key to publish the guide.
 if [ "$GUIDE_SSH_KEY" != "" ]; then
 
-    # Build the cloud version of the documentation site.
-    echo "Building online user guide."
-    npm run hosted-docs
+    # Build the cloud version of the documentation site and PDF artifact.
+    echo "Building the online and PDF user guides."
+    bundle config --local path .bundle/gems
+    bundle
+    npm run hosted-docs -- --attribute "release-tag=$release_tag"
 
     # Make sure there are no broken links in the versions we care about.
     curl https://htmltest.wjdp.uk | bash
@@ -19,21 +21,16 @@ if [ "$GUIDE_SSH_KEY" != "" ]; then
     # Publish the user guide to the right place on the Deep Symmetry web server.
     rsync -avz doc/build/site/ guides@deepsymmetry.org:/var/www/guides/beat-link-trigger/
 
+    # Rename the PDF to the name we like to use for the release artifact.
+    mv doc/build/assembler-pdf/beat-link-trigger/_exports/index.pdf BLT-guide.pdf
+
+    # Upload the PDF as a release artifact
+    if [ "$release_snapshot" = true ] ; then
+        gh release upload latest-preview "BLT-guide.pdf#User Guide (PDF)"
+    else
+        gh release upload "$release_tag" "BLT-guide.pdf#User Guide (PDF)"
+    fi
+
 else
-    echo "No SSH key present, not building online user guide."
-fi
-
-echo "Building PDF user guide."
-bundle config --local path .bundle/gems
-bundle
-npm run pdf-docs -- --attribute "release-tag=$release_tag"
-
-# Rename the disk image to the name we like to use for the release artifact.
-mv doc/build/assembler-pdf/beat-link-trigger/_exports/index.pdf BLT-guide.pdf
-
-# Upload the disk image as a release artifact
-if [ "$release_snapshot" = true ] ; then
-    gh release upload latest-preview "BLT-guide.pdf#User Guide (PDF)"
-else
-    gh release upload "$release_tag" "BLT-guide.pdf#User Guide (PDF)"
+    echo "No SSH key present, not building the user guide."
 fi
